7a8537e28055ae918a13206a6808901a1835ca5a
apple1003 <a123@123deMacBook-Pro.local>
2023-03-22 17:18:05 +0800

    use xPath syntax

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module crawler
 
 go 1.19
+
+require (
+	github.com/antchfx/htmlquery v1.3.0 // indirect
+	github.com/antchfx/xpath v1.2.3 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,31 +1,67 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
+	"errors"
 	"fmt"
+	"github.com/antchfx/htmlquery"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 	"io"
 	"net/http"
 )
 
 func main() {
 	url := "https://www.thepaper.cn/"
+	body, err := Fetch(url)
+	if err != nil {
+		fmt.Printf("read content failed: %v\n", err)
+		return
+	}
+
+	doc, err := htmlquery.Parse(bytes.NewReader(body))
+	if err != nil {
+		fmt.Println("htmlquery.Parse failed ", err)
+	}
+	nodes := htmlquery.Find(doc, `/div[@class="news_li"]/h2/a[@target="_blank"]`)
+
+	for _, node := range nodes {
+		fmt.Println("fetch card ", node.FirstChild.Data)
+	}
+}
+
+// 其获取网页的内容，检测网页的字符编码并将文本统一转换为 UTF-8 格式。
+func Fetch(url string) ([]byte, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Println("fetch url error:%v", err)
-		return
+		return nil, err
 	}
 
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		fmt.Println("Error status code:%v", resp.StatusCode)
-		return
+		return nil, errors.New(fmt.Sprintf("Error status code:%v", resp.StatusCode))
 	}
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DeterminEncoding(bodyReader)
 
-	body, err := io.ReadAll(resp.Body)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return io.ReadAll(utf8Reader)
+
+}
+
+// 读取编码方式
+func DeterminEncoding(r *bufio.Reader) encoding.Encoding {
+	bytes, err := r.Peek(1024)
+
 	if err != nil {
-		fmt.Println("read content failed:%v", err)
-		return
+		fmt.Println("fetch error:%v", err)
+		return unicode.UTF8
 	}
-	fmt.Println("body:", string(body))
+	e, _, _ := charset.DetermineEncoding(bytes, "")
 
+	return e
 }
